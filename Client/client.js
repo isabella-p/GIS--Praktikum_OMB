193c9dc89e148fcf7891a74900b16fffb9841ae3
isabella-p <92145544+isabella-p@users.noreply.github.com>
2021-12-22 14:04:11 +0100

    Abagbe

--- Client/client.js
+++ Client/client.js
@@ -5,25 +5,25 @@ var Client;
     console.log("Client läuft");
     const url = "http://127.0.0.1:3000";
     const path = "/convertDate";
-    //const myForm: HTMLFormElement = <HTMLFormElement> document.getElementById("myForm");
+    const myForm = document.getElementById("myForm");
     const sendButton = document.getElementById("send-button");
-    const addElement = document.getElementById("Elemet-hinzufuegen");
-    const dateInput = document.getElementById("datum-input");
+    //const addElement: HTMLButtonElement = <HTMLButtonElement> document.getElementById("Elemet-hinzufuegen");
+    //const dateInput: HTMLInputElement = <HTMLInputElement> document.getElementById("datum-input");
     sendButton.addEventListener("click", function (evt) {
         evt.preventDefault();
         sendForm();
     });
     //console.log(myForm, sendButton);
     async function sendForm() {
-        // let formData: FormData = new FormData(myForm);
-        // let query: URLSearchParams = new URLSearchParams(<any>formData);
-        // let urlWithQuery: string = url + path + "?" + query.toString();
-        // console.log(urlWithQuery);
-        let urlWithQuery = url + path + "?b=" + JSON.stringify(new Date(dateInput.value));
+        let formData = new FormData(myForm);
+        let query = new URLSearchParams(formData);
+        let urlWithQuery = url + path + "?" + query.toString();
+        console.log(urlWithQuery);
+        // let urlWithQuery: string = url + path + "?b=" + JSON.stringify(new Date (dateInput.value));
         let response = await fetch(urlWithQuery);
         let responseText = await response.text();
         console.log(responseText);
-        addElement.innerText = responseText;
+        //addElement.innerText = responseText;
     }
     /*async function sendJSONStringWithPost (url: RequestInfo, jsonString: string): Promise<void> {
         let response: Response = await fetch(url, {
